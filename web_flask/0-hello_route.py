0648e93ac2b8e7a7b33a2509300cc727eab0a995
joseph <jsphmariga@gmail.com>
2022-10-16 21:04:44 +0000

    Add script that starts flask web app

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Starts a Flask web application
+from flask import Flask, request
+
+
+app = Flask(__name__)
+app.url_map.strict_slashes = True
+
+
+@app.route('/')
+def index():
+    return 'Hello HBNB!'
