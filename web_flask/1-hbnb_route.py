1e95d1e4605651e9701a08326b0ea62019365a60
joseph <jsphmariga@gmail.com>
2022-10-18 02:55:18 +0000

    Add proper documentation

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -2,27 +2,27 @@
 """
 Starts a Flask web application
 """
-from flask import Flask, request
+from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def index():
     """
-    route to home
+    Displays Hello HBNB!
     """
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb', strict_slashes=False)
-def index2():
+@app.route("/hbnb", strict_slashes=False)
+def index_2():
     """
-    route to HBNB
+    Displays HBNB
     """
     return "HBNB"
 
 
 if __name__ == "__main__":
-    run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0")
