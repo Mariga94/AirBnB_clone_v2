c6acbf458584577bb1563d84330b222a1ae21323
joseph <jsphmariga@gmail.com>
2022-10-20 21:14:55 +0000

    Fix code

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -58,7 +58,7 @@ def index_6(n):
     return render_template('5-number.html', n=n)
 
 
-@app.route('/number_odd_or_even/<n>', strict_slashes=False)
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
 def index_7(n):
     """
     route 7
