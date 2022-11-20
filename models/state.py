9d95175f99f95a4eba6070462654a90486afc045
joseph <jsphmariga@gmail.com>
2022-11-20 12:52:01 +0000

    Improve state

--- models/state.py
+++ models/state.py
@@ -1,31 +1,26 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
-from models.base_model import Base
-from models.city import City
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+"""Defines the State class."""
 import models
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.city import City
+from sqlalchemy import Column
+from sqlalchemy import String
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
-    """ State class
+    """Represents a state for a MySQL database.
+    Inherits from SQLAlchemy Base and links to the MySQL table states.
     Attributes:
-        __tablename__(str): states table in MySQLdb
-        name(String): states' name
-        cities(): relationship between cities and states table
+        __tablename__ (str): The name of the MySQL table to store States.
+        name (sqlalchemy String): The name of the State.
+        cities (sqlalchemy relationship): The State-City relationship.
     """
-    if getenv("HBNB_TYPE_STORAGE") == 'db':
-        __tablename__ = 'states'
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state", cascade="delete")
-    else:
-        name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initialize state"""
-        super().__init__(*args, **kwargs)
+    __tablename__ = "states"
+    name = Column(String(128), nullable=False)
+    cities = relationship("City",  backref="state", cascade="delete")
 
     if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
