816f1c2f37bce761fb3ea0c164ca7bf889e98ef8
joseph <jsphmariga@gmail.com>
2022-11-21 01:45:46 +0000

    Declarative Table configuration

--- models/amenity.py
+++ models/amenity.py
@@ -13,6 +13,9 @@ class Amenity(BaseModel, Base):
         name (String): name of amenity
     """
     __tablename__ = 'amenities'
+    __table_args__ = (
+            {'mysql_default_charset': 'latin1'})
+
     name = Column(String(128), nullable=False)
     place_amenities = relationship("Place", secondary='place_amenity',
                                    viewonly=False)
